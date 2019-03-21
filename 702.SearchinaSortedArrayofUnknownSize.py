12332c2a2516c3608965c066840b24966414695d
Jeormeljy <jeromel.jy@hotmail.com>
2019-03-21 11:58:15 -0400

    Create 702.SearchinaSortedArrayofUnknownSize.py

---
+++ 702.SearchinaSortedArrayofUnknownSize.py
@@ -0,0 +1,63 @@
+"""
+
+702. Search in a Sorted Array of Unknown Size
+
+Given an integer array sorted in ascending order, write a function to search target in nums.  
+
+If target exists, then return its index, otherwise return -1. However, the array size is unknown to you. 
+
+You may only access the array using an ArrayReader interface, 
+
+where ArrayReader.get(k) returns the element of the array at index k (0-indexed).
+
+You may assume all integers in the array are less than 10000, and if you access the array out of bounds, 
+
+ArrayReader.get will return 2147483647.
+
+Example 1:
+
+Input: array = [-1,0,3,5,9,12], target = 9
+
+Output: 4
+
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+
+Input: array = [-1,0,3,5,9,12], target = 2
+
+Output: -1
+
+Explanation: 2 does not exist in nums so return -1
+ 
+Note:
+
+You may assume that all elements in the array are unique.
+
+The value of each element in the array will be in the range [-9999, 9999].
+
+"""
+
+class Solution(object):
+    def search(self, reader, target):
+        """
+        :type reader: ArrayReader
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = 1
+        # find left and right boundaries
+        while reader.get(right) < target:
+            left = right
+            right *= 2
+        # Binary Search in [left, right]
+        while left <= right:
+            mid = (left + right) / 2
+            if reader.get(mid) == target:
+                return mid
+            elif reader.get(mid) < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
