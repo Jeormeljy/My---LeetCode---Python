ba0fa68555f3a8589c0e4ef45379246931925151
Jeormeljy <jeromel.jy@hotmail.com>
2019-07-14 09:47:17 +0800

    Update 235.LowestCommonAncestorofaBinarySearchTree.py

--- 235.LowestCommonAncestorofaBinarySearchTree.py
+++ 235.LowestCommonAncestorofaBinarySearchTree.py
@@ -36,13 +36,13 @@ p and q are different and both values will exist in the BST.
 
 '''
 
+# Solution 1: Recursively
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
         """
@@ -53,8 +53,36 @@ class Solution(object):
         """
         if root is None or root == p or root == q:
             return root
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-        if left and right:
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
             return root
-        return left if right is None else right
+         
+# Solution 2: Iteratively
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root is None or root == p or root == q:
+            return root
+        while root:
+            if root.val < p.val and root.val < q.val:
+                root = root.right
+            elif root.val > p.val and root.val > q.val:
+                root = root.left
+            else:
+                return root
+        return None
+        
