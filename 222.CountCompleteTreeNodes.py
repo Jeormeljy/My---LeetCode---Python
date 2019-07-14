3bfc449713b9581724baa45b1dd2ad4551d33e7b
Jeormeljy <jeromel.jy@hotmail.com>
2019-07-14 18:19:54 +0800

    Create 222.CountCompleteTreeNodes.py

---
+++ 222.CountCompleteTreeNodes.py
@@ -0,0 +1,59 @@
+'''
+
+222. Count Complete Tree Nodes
+
+Given a complete binary tree, count the number of nodes.
+
+Note:
+
+Definition of a complete binary tree from Wikipedia:
+
+In a complete binary tree every level, except possibly the last, 
+
+is completely filled, and all nodes in the last level are as far left as possible. 
+
+t can have between 1 and 2h nodes inclusive at the last level h.
+
+Example:
+
+Input: 
+
+    1
+   / \
+  2   3
+ / \  /
+4  5 6
+
+Output: 6
+
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def countNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        leftHeight = self.getHeight(root.left)
+        rightHeight = self.getHeight(root.right)
+        if leftHeight == rightHeight:
+            return (1 << leftHeight) + self.countNodes(root.right)
+        else:
+            return (1 << rightHeight) + self.countNodes(root.left)
+    def getHeight(self, root):
+        if root is None:
+            return 0
+        height = 1
+        while root.left:
+            height += 1
+            root = root.left
+        return height
