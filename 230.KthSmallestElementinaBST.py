ad2ff71879daa4b1edc0837dfca9978ce2fad252
Jeormeljy <jeromel.jy@hotmail.com>
2019-07-15 15:32:21 +0800

    Update 230.KthSmallestElementinaBST.py

--- 230.KthSmallestElementinaBST.py
+++ 230.KthSmallestElementinaBST.py
@@ -42,13 +42,13 @@ How would you optimize the kthSmallest routine?
 
 '''
 
+# Solution 1
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-import sys
 class Solution(object):
     def kthSmallest(self, root, k):
         """
@@ -59,7 +59,7 @@ class Solution(object):
         if root is None:
             return 0
         index = [0]
-        result = [sys.maxint]
+        result = [0]
         self.helper(root, k, index, result)
         return result[0]
     def helper(self, root, k, index, result):
@@ -70,4 +70,59 @@ class Solution(object):
         if index[0] == k:
             result[0] = root.val
         self.helper(root.right, k, index, result)
+
+# Solution 2
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        leftCount = self.getCount(root.left)
+        if leftCount == k - 1:
+            return root.val
+        elif leftCount < k - 1:
+            return self.kthSmallest(root.right, k - leftCount - 1)
+        else:
+            return self.kthSmallest(root.left, k)
         
+        
+    def getCount(self, root):
+        if root is None:
+            return 0
+        return self.getCount(root.left) + self.getCount(root.right) + 1
+   
+# Solution 3
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        self.pushLeft(root, stack)
+        while k > 1:
+            cur = stack.pop()
+            k -= 1
+            if cur.right:
+                self.pushLeft(cur.right, stack)
+        return stack.pop().val
+        
+    def pushLeft(self, root, stack):
+        while root:
+            stack.append(root)
+            root = root.left
