4e09935ca3f0e0ba05d47aa2f885573dde1cade9
Jeormeljy <jeromel.jy@hotmail.com>
2019-08-18 19:32:20 +0800

    Create 0001.TwoSum.py

---
+++ 0001.TwoSum.py
@@ -0,0 +1,30 @@
+'''
+
+0001. TwoSum
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+'''
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        visited = {}
+        for index in xrange(len(nums)):
+            if target - nums[index] in visited:
+                return [visited[target - nums[index]], index]
+            visited[nums[index]] = index
+        return []
